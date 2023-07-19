f6af2bceff1cbc183e4fbe8ebdc455ac55df0557
Igor Polyakov <ihor.polyakov@gmail.com>
2023-07-19 12:39:24 +0700

    Basic todo-list implementation

---
+++ src/components/Help.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<details>
+    <summary>
+        Help / shortcuts / hotkeys
+    </summary>
+    <ul>
+        <li><code>Ctrl + Z</code> | <code>Ctrl + Shift + Z</code> &mdash; undo / redo</li>
+        <li>...</li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</details>
+
+<style>
+    summary {
+        cursor: help;
+    }
+</style>
