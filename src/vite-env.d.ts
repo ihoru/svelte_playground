f7abcb171ccc850da99a4403bcd055a118cc553e
Igor Polyakov <ihor.polyakov@gmail.com>
2023-08-28 21:07:01 +0700

    interface ImportMetaEnv improved

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 
 interface ImportMetaEnv {
   readonly MY_TODOIST_ACCESS_TOKEN: string;
+  readonly MY_STORAGE_URL: string;
+  readonly MY_GAP_BETWEEN_TASKS_MINUTES: number;
 }
 
 interface ImportMeta {
