e783580245abf0b20eb106fdb432f7b349edf4e9
Igor Polyakov <ihor.polyakov@gmail.com>
2023-08-28 21:06:23 +0700

    Reverted to the old longpress mechanism (without a Timeout)

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,40 +1,37 @@
 export function longpress(node: Node, threshold: number = 300) {
-    const onMouseDown = (event) => {
+    let dt: Date;
+
+    const onMouseDown = (event: MouseEvent) => {
         event.preventDefault();
-
-        const timeout = setTimeout(() => {
+        dt = new Date();
+    };
+    const onMouseUp = (event: MouseEvent) => {
+        event.preventDefault();
+        if (!dt) {
+            return;
+        }
+        const diff = (new Date()).getTime() - dt.getTime();
+        if (diff >= threshold) {
             node.dispatchEvent(new CustomEvent("longpress"));
-            removeListeners();
-        }, threshold);
-
-        const cancel = () => {
-            clearTimeout(timeout);
+        } else {
             node.dispatchEvent(new CustomEvent("shortpress"));
-            removeListeners();
-        };
-
-        const removeListeners = () => {
-            node.removeEventListener("mousemove", cancel);
-            node.removeEventListener("mouseup", cancel);
-
-            node.removeEventListener("touchmove", cancel);
-            node.removeEventListener("touchend", cancel);
-        };
-
-        node.addEventListener("mousemove", cancel);
-        node.addEventListener("mouseup", cancel);
-
-        node.addEventListener("touchmove", cancel, {passive: false});
-        node.addEventListener("touchend", cancel, {passive: false});
+        }
+        dt = null;
     };
 
     node.addEventListener("mousedown", onMouseDown);
+    node.addEventListener("mouseup", onMouseUp);
+
     node.addEventListener("touchstart", onMouseDown, {passive: false});
+    node.addEventListener("touchend", onMouseUp, {passive: false});
 
     return {
         destroy() {
             node.removeEventListener("mousedown", onMouseDown);
+            node.removeEventListener("mouseup", onMouseUp);
+
             node.removeEventListener("touchstart", onMouseDown);
+            node.removeEventListener("touchend", onMouseUp);
         },
     };
 }
